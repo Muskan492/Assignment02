6ea1b9b985fe567f8ed1d8e043920a9ed49c727f
Muskan492 <irshadalilashari5419@gmail.com>
2024-10-04 23:02:43 -0700

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,20 @@
+import React from"react";
+import Header  from "./componets/Header";
+import About from "./componets/About";
+
+//export default function Home (){
+  //return(
+    //<div>
+    //  <h1>Hello World!</h1>
+   // </div>
+  //);
+//};
+const HomePage = () =>{
+  return(
+    <div>
+      <Header/>
+      <About/>
+    </div>
+  );
+};
+export default HomePage;
